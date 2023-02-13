dd7fb21febdfe24d79964bd842f9845f580e74be
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-02-12 19:41:35 -0800

    Update Captcha.js

--- pages/captcha/Captcha.js
+++ pages/captcha/Captcha.js
@@ -1,21 +1,21 @@
 var captcha;
-function generate() {
-  
-      document.getElementById("submit").value = "";
- 
-    // Access the element to store
-    // the generated captcha
-    captcha = document.getElementById("image");
-  
-  const randomchar =
-"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  
-  for (let i = 1; i < 5; i++) {
-        uniquechar += randomchar.charAt(
-            Math.random() * randomchar.length)
-    }
- 
-    // Store generated input
-    captcha.innerHTML = uniquechar;
+function verifyNredirect() {
+  // PSEUDO CODE
+
+  SECRET_KEY = "0xaD84ccd245c08a9C237910092f9183f693B691F1"    // replace with your secret key
+  VERIFY_URL = "https://hcaptcha.com/siteverify"
+
+  // Retrieve token from post data with key 'h-captcha-response'.
+  token = request.POST_DATA['h-captcha-response']
+
+  // Build payload with secret key and token.
+  data = { 'secret': SECRET_KEY, 'response': token }
+
+  // Make POST request with data payload to hCaptcha API endpoint.
+  response = http.post(url=VERIFY_URL, data=data)
+
+  // Parse JSON from response. Check for success or error codes.
+  response_json = JSON.parse(response.content)
+  success = response_json['success']
 }
 
